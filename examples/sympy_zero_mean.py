a17a6b34212247ad6423e5c3d286261629843cc6
bchao1 <whereisbrianchao520@gmail.com>
2022-05-26 14:38:58 +0800

    add zero mean neumann example

---
+++ examples/sympy_zero_mean.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+from sympy import sin, cos
+from sympy.abc import x, y
+
+from context import poissonpy
+from poissonpy import functional, utils, solvers
+
+    
+# analytic = sin(x) + cos(y)
+# laplacian = -sin(x) - cos(y)
+# x derivative = cos(x)
+# y derivative = -sin(y)
+f_expr = sin(x) + cos(y)
+laplacian_expr = functional.get_sp_laplacian_expr(f_expr)
+x_derivative_expr = functional.get_sp_derivative_expr(f_expr, x)
+y_derivative_expr = functional.get_sp_derivative_expr(f_expr, y)
+
+f = functional.get_sp_function(f_expr)
+laplacian = functional.get_sp_function(laplacian_expr)
+
+# possible boundary conditions: neumann_x, neumann_y, dirichlet
+interior = laplacian
+left = functional.get_sp_function(x_derivative_expr)
+right = functional.get_sp_function(x_derivative_expr)
+top = functional.get_sp_function(y_derivative_expr)
+bottom = functional.get_sp_function(y_derivative_expr)
+
+boundary = {
+    "left": (left, "neumann_x"),
+    "right": (right, "neumann_x"),
+    "top": (top, "neumann_y"),
+    "bottom": (bottom, "neumann_y")
+}
+
+solver = solvers.Poisson2DRectangle(
+    ((-2*np.pi, -2*np.pi), (2*np.pi, 2*np.pi)), interior, boundary, 100, 100, zero_mean=False)
+gt = f(solver.x_grid, solver.y_grid)
+
+solution = solver.solve()
+err = np.abs(gt - solution)
+
+utils.plot_2d(solver.x, solver.y, solution, "solution")
+utils.plot_3d(solver.x_grid, solver.y_grid, solution, "solution")
+utils.plot_3d(solver.x_grid, solver.y_grid, gt, "ground truth")
+utils.plot_2d(solver.x, solver.y, err, "error")
